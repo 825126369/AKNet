083822a43d3cd0688b3c688341e9275cb6aace94
xuke <825126369@users.noreply.github.com>
2024-11-17 11:53:39 +0800

    增加  Protobuf  Reset 扩展 工具类

---
+++ AKNet/Editor/ProtobufEditor.cs
@@ -0,0 +1,128 @@
+using AKNet.Common;
+using System.Diagnostics;
+using System.IO;
+
+namespace AKNet.Editor
+{
+    public static class ProtoBufEditor
+    {
+        public static void DoProtoResetCSFile(string ProtoOutPath, string ProtoNameSpaceRootName, string ProtoDLLFilePath)
+        {
+            NetLog.Log("ProtoOutPath: " + ProtoOutPath);
+            NetLog.Log("ProtoNameSpaceRootName: " + ProtoNameSpaceRootName);
+            NetLog.Log("ProtoDLLFilePath: " + ProtoDLLFilePath);
+
+            ProtobufGenReset.GenerateProtoResetCSFile(ProtoOutPath, ProtoNameSpaceRootName, ProtoDLLFilePath);
+        }
+
+        public static void DoPublicCSFile(string ProtocExePath, string ProtoOutPath, string ProtoPath)
+        {
+            NetLog.Log("ProtocExePath: " + ProtocExePath);
+            NetLog.Log("ProtoOutPath: " + ProtoOutPath);
+            NetLog.Log("ProtoPath: " + ProtoPath);
+
+            if (!Directory.Exists(ProtoPath))
+            {
+                Directory.CreateDirectory(ProtoPath);
+            }
+
+            if (!Directory.Exists(ProtoOutPath))
+            {
+                Directory.CreateDirectory(ProtoOutPath);
+            }
+
+            string arg = $"--csharp_out={Path.GetRelativePath(ProtoPath, ProtoOutPath)} ";
+            foreach (string v in Directory.GetFiles(ProtoPath, "*.proto", SearchOption.TopDirectoryOnly))
+            {
+                arg += " " + Path.GetFileName(v);
+            }
+
+            RunCmd(Path.GetFullPath(ProtocExePath), Path.GetFullPath(ProtoPath), arg);
+        }
+        
+        public static void DoInternalCSFile(string ProtocExePath, string ProtoOutPath, string ProtoPath)
+        {
+            NetLog.Log("ProtocExePath: " + ProtocExePath);
+            NetLog.Log("ProtoOutPath: " + ProtoOutPath);
+            NetLog.Log("ProtoPath: " + ProtoPath);
+
+            if (!Directory.Exists(ProtoPath))
+            {
+                Directory.CreateDirectory(ProtoPath);
+            }
+            if (!Directory.Exists(ProtoOutPath))
+            {
+                Directory.CreateDirectory(ProtoOutPath);
+            }
+
+            string arg = $"--csharp_out={Path.GetRelativePath(ProtoPath, ProtoOutPath)} ";
+            arg += " --csharp_opt=internal_access";
+            foreach (string v in Directory.GetFiles(ProtoPath, "*.proto", SearchOption.TopDirectoryOnly))
+            {
+                arg += " " + Path.GetFileName(v);
+            }
+            RunCmd(Path.GetFullPath(ProtocExePath), Path.GetFullPath(ProtoPath), arg);
+        }
+
+        public static void DoPublicCSFile(string ProtocExePath, string ProtoOutPath, params string[] ProtoPath)
+        {
+            NetLog.Log("ProtocExePath: " + ProtocExePath);
+            NetLog.Log("ProtoOutPath: " + ProtoOutPath);
+            for (int i = 0; i < ProtoPath.Length; i++)
+            {
+                NetLog.Log("ProtoPath: " + ProtoPath);
+            }
+
+            if (!Directory.Exists(ProtoOutPath))
+            {
+                Directory.CreateDirectory(ProtoOutPath);
+            }
+
+            string arg = $"--csharp_out={Path.GetFullPath(ProtoOutPath)}";
+            for (int i = 0; i < ProtoPath.Length; i++)
+            {
+                arg += " --proto_path=" + Path.GetFullPath(ProtoPath[i]);
+            }
+
+            for (int i = 0; i < ProtoPath.Length; i++)
+            {
+                foreach (string v in Directory.GetFiles(ProtoPath[i], "*.proto", SearchOption.TopDirectoryOnly))
+                {
+                    arg += " " + Path.GetFileName(v);
+                }
+            }
+
+            RunCmd(Path.GetFullPath(ProtocExePath), null, arg);
+        }
+
+        private static void RunCmd(string exePath, string workPath, string arg)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.Arguments = arg;
+            info.WorkingDirectory = workPath;
+            info.FileName = exePath;
+            info.UseShellExecute = false;
+            info.CreateNoWindow = true;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+            info.StandardOutputEncoding = System.Text.UTF8Encoding.UTF8;
+            info.StandardErrorEncoding = System.Text.UTF8Encoding.UTF8;
+
+            NetLog.Log("RunCmd: " + info.Arguments);
+
+            Process process = Process.Start(info);
+            string strOutput = process.StandardOutput.ReadToEnd();
+            if (!string.IsNullOrWhiteSpace(strOutput))
+            {
+                NetLog.Log(strOutput);
+            }
+            strOutput = process.StandardError.ReadToEnd();
+            if (!string.IsNullOrWhiteSpace(strOutput))
+            {
+                NetLog.LogError(strOutput);
+            }
+            process.WaitForExit();
+            process.Close();
+        }
+    }
+}
---
+++ AkNetEditor/Program.cs
@@ -0,0 +1,46 @@
+﻿using AKNet.Editor;
+using System.Reflection;
+using TestCommon;
+
+namespace AkNetEditorTest
+{
+    internal class Program
+    {
+        private static string ProtocPath = GetProtocExePath();
+        private static string ProtoPath = GetProtoPath();
+        private static string ProtoOutPath = GetProtoOutPath();
+
+        private static string ProtoNameSpaceRootName = "NetProtocols";
+        private static string ProtoDLLFilePath = GetProtoDLLFilePath();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ProtoBufEditor.DoPublicCSFile(ProtocPath, ProtoOutPath, ProtoPath);
+            ProtoBufEditor.DoProtoResetCSFile(ProtoOutPath, ProtoNameSpaceRootName, ProtoDLLFilePath);
+        }
+
+
+        static string GetProtocExePath()
+        {
+            return Path.Combine(FileTool.GetParentSpecialDir("protoc-28.2-win64"), "bin", "protoc.exe");
+        }
+
+        static string GetProtoPath()
+        {
+            return Path.Combine(FileTool.GetParentSpecialDir("Protobuf"));
+        }
+
+        static string GetProtoOutPath()
+        {
+            return Path.Combine(GetProtoPath(), "Out");
+        }
+
+        static string GetProtoDLLFilePath()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            return assembly.GetName().Name + ".dll";
+        }
+    }
+}
+
