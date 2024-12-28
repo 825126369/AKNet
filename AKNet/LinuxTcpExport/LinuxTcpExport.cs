6eea2900f34e05b26861847e9ab271091c0d763d
xuke <825126369@users.noreply.github.com>
2024-12-29 04:59:20 +0800

    2

---
+++ AKNet/LinuxTcpExport/LinuxTcpExport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace AKNet.LinuxTcp
+{
+    internal static partial class LinuxTcpFunc
+    {
+        public static void SendTcpStream(tcp_sock tp, ReadOnlySpan<byte> buffer)
+        {
+
+        }
+
+        public static void MultiThreading_ReceiveWaitCheckNetPackage(tcp_sock tp, SocketAsyncEventArgs e)
+        {
+            ReadOnlySpan<byte> mBuff = e.MemoryBuffer.Span.Slice(e.Offset, e.BytesTransferred);
+            
+        }
+
+    }
+}
