78b44f718952960b2e0e858523acf3e70000f83a
xuke <825126369@users.noreply.github.com>
2025-01-13 09:21:30 +0800

    2

--- AKNet/LinuxTcp/rbtree.cs
+++ AKNet/LinuxTcp/rbtree.cs
@@ -3,6 +3,27 @@ using System.Xml.Linq;
 
 namespace AKNet.LinuxTcp
 {
+    /*
+        红黑树（Red-Black Tree）是一种自平衡的二叉搜索树，它在每个节点上增加了一个存储位来表示节点的颜色，可以是红色或黑色。
+        通过对任何一条从根到叶子的路径上节点的颜色进行约束，红黑树确保树的平衡，从而保证了基本操作（如查找、插入和删除）的时间复杂度为 O(log n)。
+        
+        红黑树的性质
+        红黑树具有以下五个基本性质：
+        1: 节点是红色或黑色：每个节点都有一个颜色属性，红色或黑色。
+        2: 根节点是黑色：树的根节点必须是黑色。
+        3: 叶子节点是黑色：叶子节点（即空节点或 NULL 节点）是黑色。
+        4: 红色节点的子节点是黑色：如果一个节点是红色，则它的两个子节点都是黑色。
+        5: 从任何节点到其每个叶子节点的所有路径都包含相同数量的黑色节点。
+        为什么需要红黑树
+        红黑树是一种自平衡的二叉搜索树，它通过颜色约束来确保树的平衡。
+        与 AVL 树相比，红黑树在插入和删除操作中进行的调整较少，因此在实际应用中，红黑树的插入和删除操作通常比 AVL 树更快。
+        红黑树在许多编程语言的标准库中被广泛使用，例如 C++ 的 std::map 和 std::set，以及 Linux 内核中的内存管理、文件系统等。
+        红黑树的操作
+        查找：与普通二叉搜索树的查找操作相同，时间复杂度为 O(log n)。
+        插入：插入新节点后，需要进行颜色调整和旋转操作，以保持红黑树的性质。
+        删除：删除节点后，也需要进行颜色调整和旋转操作，以保持红黑树的性质。
+     */
+
     internal class rb_node
     {
         public sk_buff value;
@@ -259,7 +280,210 @@ namespace AKNet.LinuxTcp
             }
         }
 
+        static void ____rb_erase_color(rb_node parent, rb_root root, Action<rb_node, rb_node> augment_rotate)
+        {
+            rb_node node = null, sibling, tmp1, tmp2;
 
+            while (true)
+            {
+                /*
+		         * Loop invariants:
+		         * - node is black (or NULL on first iteration)
+		         * - node is not the root (parent is not NULL)
+		         * - All leaf paths going through parent and node have a
+		         *   black node count that is 1 lower than other leaf paths.
+		         */
+                sibling = parent.rb_right;
+                if (node != sibling)
+                {
+                    /* node == parent->rb_left */
+                    if (rb_is_red(sibling))
+                    {
+                        /*
+				         * Case 1 - left rotate at parent
+				         *
+				         *     P               S
+				         *    / \             / \
+				         *   N   s    -->    p   Sr
+				         *      / \         / \
+				         *     Sl  Sr      N   Sl
+				         */
+                        tmp1 = sibling.rb_left;
+                        parent.rb_right = tmp1;
+                        sibling.rb_left = parent;
+                        rb_set_parent_color(tmp1, parent, RB_BLACK);
+                        __rb_rotate_set_parents(parent, sibling, root, RB_RED);
+                        augment_rotate(parent, sibling);
+                        sibling = tmp1;
+                    }
+                    tmp1 = sibling.rb_right;
+                    if (tmp1 == null || rb_is_black(tmp1))
+                    {
+                        tmp2 = sibling.rb_left;
+                        if (tmp2 == null || rb_is_black(tmp2))
+                        {
+                            /*
+					         * Case 2 - sibling color flip
+					         * (p could be either color here)
+					         *
+					         *    (p)           (p)
+					         *    / \           / \
+					         *   N   S    -->  N   s
+					         *      / \           / \
+					         *     Sl  Sr        Sl  Sr
+					         *
+					         * This leaves us violating 5) which
+					         * can be fixed by flipping p to black
+					         * if it was red, or by recursing at p.
+					         * p is red when coming from Case 1.
+					         */
+                            rb_set_parent_color(sibling, parent, RB_RED);
+                            if (rb_is_red(parent))
+                            {
+                                rb_set_black(parent);
+                            }
+                            else
+                            {
+                                node = parent;
+                                parent = rb_parent(node);
+                                if (parent != null)
+                                {
+                                    continue;
+                                }
+                            }
+                            break;
+                        }
+
+                        /*
+				         * Case 3 - right rotate at sibling
+				         * (p could be either color here)
+				         *
+				         *   (p)           (p)
+				         *   / \           / \
+				         *  N   S    -->  N   sl
+				         *     / \             \
+				         *    sl  sr            S
+				         *                       \
+				         *                        sr
+				         *
+				         * Note: p might be red, and then both
+				         * p and sl are red after rotation(which
+				         * breaks property 4). This is fixed in
+				         * Case 4 (in __rb_rotate_set_parents()
+				         *         which set sl the color of p
+				         *         and set p RB_BLACK)
+				         *
+				         *   (p)            (sl)
+				         *   / \            /  \
+				         *  N   sl   -->   P    S
+				         *       \        /      \
+				         *        S      N        sr
+				         *         \
+				         *          sr
+				         */
+                        tmp1 = tmp2.rb_right;
+                        sibling.rb_left = tmp1;
+                        tmp2.rb_right = sibling;
+                        parent.rb_right = tmp2;
+                        if (tmp1 != null)
+                        {
+                            rb_set_parent_color(tmp1, sibling, RB_BLACK);
+                        }
+                        augment_rotate(sibling, tmp2);
+                        tmp1 = sibling;
+                        sibling = tmp2;
+                    }
+                    /*
+			         * Case 4 - left rotate at parent + color flips
+			         * (p and sl could be either color here.
+			         *  After rotation, p becomes black, s acquires
+			         *  p's color, and sl keeps its color)
+			         *
+			         *      (p)             (s)
+			         *      / \             / \
+			         *     N   S     -->   P   Sr
+			         *        / \         / \
+			         *      (sl) sr      N  (sl)
+			         */
+                    tmp2 = sibling.rb_left;
+                    parent.rb_right = tmp2;
+                    sibling.rb_left = parent;
+                    rb_set_parent_color(tmp1, sibling, RB_BLACK);
+                    if (tmp2 != null)
+                    {
+                        rb_set_parent(tmp2, parent);
+                    }
+                    __rb_rotate_set_parents(parent, sibling, root, RB_BLACK);
+                    augment_rotate(parent, sibling);
+                    break;
+                }
+                else
+                {
+                    sibling = parent.rb_left;
+                    if (rb_is_red(sibling))
+                    {
+                        /* Case 1 - right rotate at parent */
+                        tmp1 = sibling.rb_right;
+                        parent.rb_left = tmp1;
+                        sibling.rb_right = parent;
+                        rb_set_parent_color(tmp1, parent, RB_BLACK);
+                        __rb_rotate_set_parents(parent, sibling, root, RB_RED);
+                        augment_rotate(parent, sibling);
+                        sibling = tmp1;
+                    }
+
+                    tmp1 = sibling.rb_left;
+                    if (tmp1 == null || rb_is_black(tmp1))
+                    {
+                        tmp2 = sibling.rb_right;
+                        if (tmp2 == null || rb_is_black(tmp2))
+                        {
+                            /* Case 2 - sibling color flip */
+                            rb_set_parent_color(sibling, parent, RB_RED);
+                            if (rb_is_red(parent))
+                            {
+                                rb_set_black(parent);
+                            }
+                            else
+                            {
+                                node = parent;
+                                parent = rb_parent(node);
+                                if (parent != null)
+                                {
+                                    continue;
+                                }
+                            }
+                            break;
+                        }
+                        /* Case 3 - left rotate at sibling */
+                        tmp1 = tmp2.rb_left;
+                        sibling.rb_right = tmp1;
+                        tmp2.rb_left = sibling;
+                        parent.rb_left = tmp2;
+                        if (tmp1 != null)
+                        {
+                            rb_set_parent_color(tmp1, sibling, RB_BLACK);
+                        }
+                        augment_rotate(sibling, tmp2);
+                        tmp1 = sibling;
+                        sibling = tmp2;
+                    }
+
+                    /* Case 4 - right rotate at parent + color flips */
+                    tmp2 = sibling.rb_right;
+                    parent.rb_left = tmp2;
+                    sibling.rb_right = parent;
+                    rb_set_parent_color(tmp1, sibling, RB_BLACK);
+                    if (tmp2 != null)
+                    {
+                        rb_set_parent(tmp2, parent);
+                    }
+                    __rb_rotate_set_parents(parent, sibling, root, RB_BLACK);
+                    augment_rotate(parent, sibling);
+                    break;
+                }
+            }
+        }
 
 
     }
