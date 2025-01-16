e0dd61d4c8b1ce9bfee51e0bb20df19d78be13e8
xuke <825126369@users.noreply.github.com>
2025-01-16 10:57:35 +0800

    2

--- AKNet.LinuxTcp/LinuxTcp/tcp_input.cs
+++ AKNet.LinuxTcp/LinuxTcp/tcp_input.cs
@@ -1063,21 +1063,19 @@ namespace AKNet.LinuxTcp
         static void tcp_sack_maybe_coalesce(tcp_sock tp)
         {
 	        int this_sack;
-            int spIndex = 0;
-            tcp_sack_block sp = tp.selective_acks[spIndex];
-
-            int swalkIndex = spIndex + 1;
+            tcp_sack_block[] sp = tp.selective_acks;
+            int swalkIndex = 1;
             tcp_sack_block swalk = tp.selective_acks[swalkIndex];
 
             for (this_sack = 1; this_sack < tp.rx_opt.num_sacks;)
             {
-                if (tcp_sack_extend(sp, swalk.start_seq, swalk.end_seq))
+                if (tcp_sack_extend(sp[0], swalk.start_seq, swalk.end_seq))
                 {
                     int i;
                     tp.rx_opt.num_sacks--;
                     for (i = this_sack; i < tp.rx_opt.num_sacks; i++)
                     {
-                        tp.selective_acks[spIndex] = tp.selective_acks[spIndex + 1];
+                        sp[i] = sp[i + 1];
                     }
                     continue;
                 }
