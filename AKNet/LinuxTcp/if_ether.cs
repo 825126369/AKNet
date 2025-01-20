200a2cc4e10eca5cf6531770bab954080924927b
xuke <825126369@users.noreply.github.com>
2025-01-20 23:13:25 +0800

    2

--- AKNet/LinuxTcp/if_ether.cs
+++ AKNet/LinuxTcp/if_ether.cs
@@ -1,5 +1,9 @@
-﻿namespace AKNet.LinuxTcp
+﻿using AKNet.Common;
+using System;
+
+namespace AKNet.LinuxTcp
 {
+
     internal class ethhdr
     {
         public const int ETH_ALEN = 6;		/* Octets in one ethernet addr	 */
@@ -7,5 +11,20 @@
         public byte[] h_dest = new byte[ETH_ALEN]; //目的 MAC 地址
         public byte[] h_source = new byte[ETH_ALEN]; //源 MAC 地址
         public ushort h_proto;     //协议类型字段
+
+        public void WriteTo(Span<byte> mBuffer)
+        {
+            h_dest.CopyTo(mBuffer);
+            h_source.CopyTo(mBuffer);
+            EndianBitConverter.SetBytes(mBuffer, 12, h_proto);
+        }
+
+        public void WriteFrom(ReadOnlySpan<byte> mBuffer)
+        {
+            h_dest = mBuffer.Slice(0, ETH_ALEN).ToArray();
+            h_source = mBuffer.Slice(ETH_ALEN, ETH_ALEN).ToArray();
+            h_proto = EndianBitConverter.ToUInt16(mBuffer, 12);
+        }
+
     }
 }
