c21fa1d2e9e0482ba765bd2619de13be295e02a9
xuke <825126369@users.noreply.github.com>
2025-01-22 15:24:35 +0800

    2

--- AKNet/LinuxTcp/slab.cs
+++ AKNet/LinuxTcp/slab.cs
@@ -1,54 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net.Sockets;
 
 namespace AKNet.LinuxTcp
 {
     internal class SlabManager
     {
-        readonly byte[] m_buffer;
-        readonly Memory<byte> m_memory;
-        readonly Stack<int> m_freeIndexPool;
-        readonly int nBufferSize = 0;
+        readonly byte[] mBuffer;
+        readonly Stack<int> m_freeIndexPool = new Stack<int>();
+        readonly int nBufferSegmentSize = 0;
         int nReadIndex = 0;
 
-        public SlabManager(int nBufferSize, int nCount)
+        public SlabManager(int nBufferSegmentSize, int nMaxCount)
         {
-            this.nBufferSize = nBufferSize;
-            this.m_freeIndexPool = new Stack<int>();
-            int Length = nBufferSize * nCount;
-            this.m_buffer = new byte[Length];
-            this.m_memory = m_buffer;
-
+            this.nBufferSegmentSize = nBufferSegmentSize;
+            this.mBuffer = new byte[nBufferSegmentSize * nMaxCount];
             this.nReadIndex = 0;
         }
 
-        public Span<byte> AllocBuffer()
+        public ArraySegment<byte> AllocBuffer()
         {
-            Span<byte> mAllocBuffer = m_memory.Span;
             if (m_freeIndexPool.Count > 0)
             {
-                mAllocBuffer = mAllocBuffer.Slice(m_freeIndexPool.Pop(), nBufferSize);
+                ArraySegment<byte> mAllocBuffer = new ArraySegment<byte>(mBuffer, m_freeIndexPool.Pop(), nBufferSegmentSize);
+                return mAllocBuffer;
             }
             else
             {
-                if (nReadIndex + nBufferSize <= m_buffer.Length)
+                if (nReadIndex + nBufferSegmentSize <= mBuffer.Length)
                 {
-                    mAllocBuffer = mAllocBuffer.Slice(nReadIndex, nBufferSize);
-                    nReadIndex += nBufferSize;
+                    ArraySegment<byte> mAllocBuffer = new ArraySegment<byte>(mBuffer, nReadIndex, nBufferSegmentSize);
+                    nReadIndex += nBufferSegmentSize;
+                    return mAllocBuffer;
                 }
                 else
                 {
-                    return Span<byte>.Empty;
+                    return ArraySegment<byte>.Empty;
                 }
             }
-            return mAllocBuffer;
         }
 
-        public void FreeBuffer()
+        public void FreeBuffer(int nOffset)
         {
-            //m_freeIndexPool.Push(args.Offset);
-            //args.(null);
+            m_freeIndexPool.Push(nOffset);
+        }
+
+        public void FreeBuffer(ArraySegment<byte> mArraySegment)
+        {
+            m_freeIndexPool.Push(mArraySegment.Offset);
         }
 
     }
