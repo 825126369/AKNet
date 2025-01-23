3a814f0a87b85478748f40d902c09b130424234c
xuke <825126369@users.noreply.github.com>
2025-01-23 11:14:44 +0800

    2

--- AKNet/LinuxTcp/ip_output.cs
+++ AKNet/LinuxTcp/ip_output.cs
@@ -22,6 +22,8 @@ namespace AKNet.LinuxTcp
             skb_set_mac_header(skb, sizeof_ethhdr);
             skb_set_network_header(skb, sizeof_iphdr);
 
+            ip_hdr(skb).tot_len = (ushort)skb.len;
+
             Span<byte> mBuffer = skb.mBuffer.AsSpan().Slice(skb.mac_header);
             eth_hdr(skb).WriteTo(mBuffer);
 
@@ -30,6 +32,29 @@ namespace AKNet.LinuxTcp
 
 			IPLayerSendStream(tp, skb);
             return 0;
+        }
+
+        static int __ip_local_out(net net, tcp_sock tp, sk_buff skb)
+        {
+	            iphdr iph = ip_hdr(skb);
+
+                IP_INC_STATS(net, IPSTATS_MIB_OUTREQUESTS);
+                
+                iph_set_totlen(iph, skb->len);
+                ip_send_check(iph);
+
+                /* if egress device is enslaved to an L3 master device pass the
+                 * skb to its handler for processing
+                 */
+                skb = l3mdev_ip_out(sk, skb);
+	        if (unlikely(!skb))
+		        return 0;
+
+	        skb->protocol = htons(ETH_P_IP);
+
+	        return nf_hook(NFPROTO_IPV4, NF_INET_LOCAL_OUT,
+                       net, sk, skb, NULL, skb_dst(skb)->dev,
+                       dst_output);
         }
 
     }
