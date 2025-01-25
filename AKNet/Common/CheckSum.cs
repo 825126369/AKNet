6ea2f60ca3461ff8d78d500684ffbc418567318d
xuke <825126369@users.noreply.github.com>
2025-01-25 23:51:08 +0800

    使用 腾讯元宝大模型，生成的CheckSum 感觉还不错啊

--- AKNet/Common/CheckSum.cs
+++ AKNet/Common/CheckSum.cs
@@ -4,13 +4,7 @@ namespace AKNet.Common
 {
     public static class CheckSumHelper
     {
-        /// <summary>
-        /// 计算部分数据的校验和，并结合之前的校验和。
-        /// </summary>
-        /// <param name="buff">数据缓冲区。</param>
-        /// <param name="len">数据长度（字节）。</param>
-        /// <param name="wsum">之前的校验和。</param>
-        /// <returns>新的校验和。</returns>
+        // 计算部分数据的校验和，并结合之前的校验和。
         public static ushort CsumPartial(byte[] buff, int len, ushort wsum)
         {
             uint sum = wsum;
@@ -26,12 +20,7 @@ namespace AKNet.Common
             return (ushort)result;
         }
 
-        /// <summary>
-        /// 计算整个数据缓冲区的校验和。
-        /// </summary>
-        /// <param name="buff">数据缓冲区。</param>
-        /// <param name="len">数据长度（字节）。</param>
-        /// <returns>校验和。</returns>
+        // 计算整个数据缓冲区的校验和。
         public static ushort DoCsum(byte[] buff, int len)
         {
             if (buff == null || len <= 0)
@@ -100,11 +89,7 @@ namespace AKNet.Common
             return (ushort)result;
         }
 
-        /// <summary>
-        /// 将 32 位校验和压缩到 16 位，并处理字节序。
-        /// </summary>
-        /// <param name="sum">32 位校验和。</param>
-        /// <returns>16 位校验和。</returns>
+        //将32位校验和压缩到 16 位，并处理字节序。
         private static uint CsumFrom32To16(uint sum)
         {
             while (sum >> 16 != 0)
@@ -112,6 +97,60 @@ namespace AKNet.Common
                 sum = (sum & 0xFFFF) + (sum >> 16);
             }
             return sum;
+        }
+
+        //将64位值折叠为 32 位校验和。
+        private static uint From64To32(ulong s)
+        {
+            // 如果高 32 位不为零，则将其加到低 32 位
+            if ((s >> 32) != 0)
+            {
+                s = (s & 0xFFFFFFFF) + (s >> 32);
+            }
+            // 如果结果超过 16 位，则再次折叠
+            if ((s >> 16) != 0)
+            {
+                s = (s & 0xFFFF) + (s >> 16);
+            }
+            return (uint)s;
+        }
+
+        public static uint CsumTcpUdpNofold(uint saddr, uint daddr, uint len, byte proto, uint sum)
+        {
+            // 将所有部分相加
+            ulong s = sum;
+
+            s += saddr;
+            s += daddr;
+
+            // 根据字节序添加协议类型和长度
+            if (BitConverter.IsLittleEndian)
+            {
+                s += proto + len;
+            }
+            else
+            {
+                s += (proto + len) << 8;
+            }
+
+            // 返回未折叠的 32 位结果
+            return (uint)s;
+        }
+
+        // 计算 TCP 或 UDP 校验和（完整版本）。
+        public static ushort ComputeTcpUdpChecksum(byte[] buff, int len, uint saddr, uint daddr, byte proto)
+        {
+            // 计算部分校验和
+            uint partialSum = CsumTcpUdpNofold(saddr, daddr, len, proto, 0);
+
+            // 计算数据部分的校验和
+            uint dataSum = DoCsum(buff, len);
+
+            // 合并两部分校验和
+            uint totalSum = partialSum + dataSum;
+
+            // 折叠为 16 位校验和
+            return (ushort)From64To32(totalSum);
         }
     }
 }
