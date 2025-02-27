daf6bf4cfc04e3a18f8167947e4fd8869811d51c
xuke <825126369@users.noreply.github.com>
2025-02-27 22:29:15 +0800

    2

--- AKNet/Tcp/Common/CryptoMgr/CryptoMgr.cs
+++ AKNet/Tcp/Common/CryptoMgr/CryptoMgr.cs
@@ -3,7 +3,7 @@
 *        Web:https://github.com/825126369/AKNet
 *        Description:这是一个面向 .Net Standard 2.1 的游戏网络库
 *        Author:阿珂
-*        CreateTime:2024/11/28 7:14:05
+*        ModifyTime:2025/2/27 22:28:11
 *        Copyright:MIT软件许可证
 ************************************Copyright*****************************************/
 using AKNet.Common;
