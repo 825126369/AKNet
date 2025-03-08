f4fb1c783c9e6fbee136334c6f4374e96b30f7cb
xuke <825126369@users.noreply.github.com>
2025-03-08 20:58:20 +0800

    Quic

--- AKNet/Udp5Quic/msquic/listener.cs
+++ AKNet/Udp5Quic/msquic/listener.cs
@@ -1,4 +1,6 @@
-﻿namespace AKNet.Udp5Quic.Common
+﻿using System.Collections.Generic;
+
+namespace AKNet.Udp5Quic.Common
 {
     internal class QUIC_LISTENER
     {
@@ -6,19 +8,19 @@
         public bool AppClosed;
         public bool Stopped;
         public bool NeedsCleanup;
-        CXPLAT_THREAD_ID StopCompleteThreadID;
-        CXPLAT_LIST_ENTRY Link;
-        QUIC_REGISTRATION* Registration;
-        CXPLAT_LIST_ENTRY RegistrationLink;
-        CXPLAT_REF_COUNT RefCount;
-        CXPLAT_EVENT StopEvent;
-        QUIC_ADDR LocalAddress;
-        QUIC_BINDING* Binding;
-        QUIC_LISTENER_CALLBACK_HANDLER ClientCallbackHandler;
-        uint64_t TotalAcceptedConnections;
-        uint64_t TotalRejectedConnections;
-        uint16_t AlpnListLength;
-        uint8_t* AlpnList;
+        // CXPLAT_THREAD_ID StopCompleteThreadID;
+        //CXPLAT_LIST_ENTRY Link;
+        //QUIC_REGISTRATION* Registration;
+        //CXPLAT_LIST_ENTRY RegistrationLink;
+        //CXPLAT_REF_COUNT RefCount;
+        //CXPLAT_EVENT StopEvent;
+        public string LocalAddress;
+        //QUIC_BINDING* Binding;
+        //QUIC_LISTENER_CALLBACK_HANDLER ClientCallbackHandler;
+
+        public ulong TotalAcceptedConnections;
+        public ulong TotalRejectedConnections;
+        public readonly List<byte> AlpnList = new List<byte>();
         public byte[] CibirId = new byte[2 + MSQuicFunc.QUIC_MAX_CIBIR_LENGTH];
     }
 }
---
+++ AKNet/Udp5Quic/msquic/msquic.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AKNet.Udp5Quic.Common
+{
+   // public delegate void QUIC_LISTENER_CALLBACK_HANDLER(QUIC_LISTENER Listener, IntPtr Context, ref QUIC_NEW_CONNECTION_INFO Info);
+}
