9afb2f621b347a99b0e7379903fcb86f3703722f
xuke <825126369@qq.com>
2025-07-10 21:12:14 +0800

    2

--- AKNet/Udp5MSQuic/msquicExport/MsQuicApi.cs
+++ AKNet/Udp5MSQuic/msquicExport/MsQuicApi.cs
@@ -16,18 +16,23 @@ namespace AKNet.Udp5MSQuic.Common
                 if (mInstance == null)
                 {
                     mInstance = new MsQuicApi();
+                    if (!mInstance.CheckAndInit())
+                    {
+                        return null;
+                    }
+                    
                 }
                 return mInstance;
             }
         }
 
-        private MsQuicApi()
+        private bool CheckAndInit()
         {
             MSQuicFunc.DoTest();
             if (MSQuicFunc.QUIC_FAILED(MSQuicFunc.MsQuicOpenVersion((uint)s_minMsQuicVersion.Major, out _)))
             {
                 NetLog.LogError("MSQuicFunc.MsQuicOpenVersion Error");
-                return;
+                return false;
             }
 
             var cfg = new QUIC_REGISTRATION_CONFIG
@@ -39,7 +44,9 @@ namespace AKNet.Udp5MSQuic.Common
             if (MSQuicFunc.QUIC_FAILED(MSQuicFunc.MsQuicRegistrationOpen(cfg, out Registration)))
             {
                 NetLog.LogError("MsQuicRegistrationOpen Fail");
+                return false;
             }
+            return true;
         }
     }
 }
