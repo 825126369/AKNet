2822fa18b5b1a11bb012bb363cbe2cfedc72ced3
xuke <825126369@qq.com>
2025-07-10 21:08:39 +0800

    2

--- AKNet/Udp5MSQuic/msquicExport/MsQuicApi.cs
+++ AKNet/Udp5MSQuic/msquicExport/MsQuicApi.cs
@@ -5,26 +5,31 @@ namespace AKNet.Udp5MSQuic.Common
 {
     internal sealed class MsQuicApi
     {
-        private static readonly Lazy<MsQuicApi> _api = new Lazy<MsQuicApi>(AllocateMsQuicApi);
+        private static MsQuicApi mInstance;
         public QUIC_REGISTRATION Registration;
-
-        internal static string MsQuicLibraryVersion { get; } = "unknown";
-        internal static string? NotSupportedReason { get; }
-        internal static bool UsesSChannelBackend { get; }
-        internal static bool Tls13ServerMayBeDisabled { get; }
-        internal static bool Tls13ClientMayBeDisabled { get; }
-
-        private static readonly Version s_minWindowsVersion = new Version(10, 0, 20145, 1000);
-        private static readonly Version s_minMsQuicVersion = new Version(2, 2, 2);
+        private static readonly Version s_minMsQuicVersion = new Version(2, 0, 0);
         
         public static MsQuicApi Api
         {
-            get { return _api.Value; }
+            get
+            {
+                if (mInstance == null)
+                {
+                    mInstance = new MsQuicApi();
+                }
+                return mInstance;
+            }
         }
 
         private MsQuicApi()
         {
             MSQuicFunc.DoTest();
+            if (MSQuicFunc.QUIC_FAILED(MSQuicFunc.MsQuicOpenVersion((uint)s_minMsQuicVersion.Major, out _)))
+            {
+                NetLog.LogError("MSQuicFunc.MsQuicOpenVersion Error");
+                return;
+            }
+
             var cfg = new QUIC_REGISTRATION_CONFIG
             {
                 AppName = "AKNet.Quic",
@@ -36,25 +41,5 @@ namespace AKNet.Udp5MSQuic.Common
                 NetLog.LogError("MsQuicRegistrationOpen Fail");
             }
         }
-
-        private static MsQuicApi AllocateMsQuicApi()
-        {
-            TryOpenMsQuic(out QUIC_API_TABLE apiTable, out int openStatus);
-            return new MsQuicApi();
-        }
-
-        private static bool TryOpenMsQuic(out QUIC_API_TABLE apiTable, out int openStatus)
-        {
-            apiTable = null;
-            openStatus = MSQuicFunc.MsQuicOpenVersion((uint)s_minMsQuicVersion.Major, out apiTable);
-            if (MSQuicFunc.QUIC_FAILED(openStatus))
-            {
-                NetLog.LogError("MSQuicFunc.MsQuicOpenVersion Error");
-                return false;
-            }
-            return true;
-        }
-
-
     }
 }
