828c9cef2942fc638e595fdba501385962ee0237
xuke <825126369@qq.com>
2025-07-24 02:54:34 +0800

    2

--- AKNet.Extentions.Protobuf/ClientPeerBaseExtentions.cs
+++ AKNet.Extentions.Protobuf/ClientPeerBaseExtentions.cs
@@ -7,21 +7,21 @@
 *        Copyright:MIT软件许可证
 ************************************Copyright*****************************************/
 
-using AKNet.Common;
-using Google.Protobuf;
-using System;
+//using AKNet.Common;
+//using Google.Protobuf;
+//using System;
 
-namespace AKNet.Extentions.Protobuf
-{
-    public static class ClientPeerBaseExtentions
-    {
-        public static void SendNetData(this ClientPeerBase mInterface, ushort nPackageId, IMessage data)
-        {
-            if (mInterface.GetSocketState() == SOCKET_PEER_STATE.CONNECTED)
-            {
-                ReadOnlySpan<byte> stream = Protocol3Utility.SerializePackage(data);
-                mInterface.SendNetData(nPackageId, stream);
-            }
-        }
-    }
-}
+//namespace AKNet.Extentions.Protobuf
+//{
+//    public static class ClientPeerBaseExtentions
+//    {
+//        public static void SendNetData(this ClientPeerBase mInterface, ushort nPackageId, IMessage data)
+//        {
+//            if (mInterface.GetSocketState() == SOCKET_PEER_STATE.CONNECTED)
+//            {
+//                ReadOnlySpan<byte> stream = Protocol3Utility.SerializePackage(data);
+//                mInterface.SendNetData(nPackageId, stream);
+//            }
+//        }
+//    }
+//}
--- Test/AkNet2Test/TestNet2Client/Program.cs
+++ Test/AkNet2Test/TestNet2Client/Program.cs
@@ -1,8 +1,21 @@
-﻿using TestCommon;
-using System.Net.WebSockets;
+﻿using AKNet.Common;
+using Google.Protobuf;
+using TestCommon;
 
 namespace TestNetClient
 {
+    public static class ClientPeerBaseExtentions
+    {
+        public static void SendNetData(this ClientPeerBase mInterface, ushort nPackageId, IMessage data)
+        {
+            if (mInterface.GetSocketState() == SOCKET_PEER_STATE.CONNECTED)
+            {
+                ReadOnlySpan<byte> stream = Protocol3Utility.SerializePackage(data);
+                mInterface.SendNetData(nPackageId, stream);
+            }
+        }
+    }
+
     internal class Program
     {
         static NetHandler mTest = null;
