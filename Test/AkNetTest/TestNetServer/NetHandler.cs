df48ffd1e9cefa62bdeeed9af7e8fb5df4cdf0a9
xuke <825126369@qq.com>
2025-07-24 01:08:30 +0800

    2

---
+++ Test/AkNetTest/TestNetServer/NetHandler.cs
@@ -0,0 +1,42 @@
+﻿using AKNet.Common;
+using AKNet.Extentions.Protobuf;
+using TestProtocol;
+
+namespace TestNetServer
+{
+    public class NetHandler
+    {
+        NetServerMain mNetServer = null;
+        const int NetCommand_COMMAND_TESTCHAT = 1000;
+
+        public const bool InTest = true;
+        private double fDuringTime;
+        public void Init()
+        {
+            mNetServer = new NetServerMain(NetType.Udp5MSQuic);
+            mNetServer.addNetListenFunc(NetCommand_COMMAND_TESTCHAT, ReceiveMessage);
+            mNetServer.InitNet(6000);
+        }
+
+        public void Update(double fElapsedTime)
+        {
+            mNetServer.Update(fElapsedTime);
+
+            fDuringTime += fElapsedTime;
+            if (fDuringTime > 5.0)
+            {
+                fDuringTime = 0.0;
+            }
+
+        }
+
+        private void ReceiveMessage(ClientPeerBase peer, NetPackage mPackage)
+        {
+            TESTChatMessage mdata = Protocol3Utility.getData<TESTChatMessage>(mPackage);
+            //Console.WriteLine("ReceiveMessage: " + mdata.TalkMsg);
+            peer.SendNetData(NetCommand_COMMAND_TESTCHAT, mdata);
+            IMessagePool<TESTChatMessage>.recycle(mdata);
+        }
+    }
+}
+
