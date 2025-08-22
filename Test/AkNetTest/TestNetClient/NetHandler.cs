664cef60423bda9b1311ca2e203ec692c6b51698
xuke <825126369@qq.com>
2025-08-22 20:39:12 +0800

    2

--- Test/AkNetTest/TestNetClient/NetHandler.cs
+++ Test/AkNetTest/TestNetClient/NetHandler.cs
@@ -8,9 +8,9 @@ namespace TestNetClient
 {
     public class NetHandler
     {
-        public const int nClientCount = 1;
-        public const int nPackageCount = 1;
-        public const double fFrameInternalTime = 1;
+        public const int nClientCount = 100;
+        public const int nPackageCount = 100;
+        public const double fFrameInternalTime = 0;
         public const int nSumPackageCount = nClientCount * nPackageCount * 100;
         int nReceivePackageCount = 0;
         List<NetClientMain> mClientList = new List<NetClientMain>();
@@ -116,7 +116,7 @@ namespace TestNetClient
             TESTChatMessage mdata = Proto3Tool.GetData<TESTChatMessage>(mPackage);
 
             nReceivePackageCount++;
-            if (nReceivePackageCount % 1 == 0)
+            if (nReceivePackageCount % 1000 == 0)
             {
                 string msg = $"接受包数量: {nReceivePackageCount} 总共花费时间: {mStopWatch.Elapsed.TotalSeconds},平均1秒发送：{nReceivePackageCount / mStopWatch.Elapsed.TotalSeconds}";
                 Console.WriteLine(msg);
--- Test/AkNetTest/TestNetServer/NetHandler.cs
+++ Test/AkNetTest/TestNetServer/NetHandler.cs
@@ -33,7 +33,6 @@ namespace TestNetServer
         private void ReceiveMessage(ClientPeerBase peer, NetPackage mPackage)
         {
             TESTChatMessage mdata = Proto3Tool.GetData<TESTChatMessage>(mPackage);
-            Console.WriteLine("ReceiveMessage: " + mdata.TalkMsg);
             peer.SendNetData(NetCommand_COMMAND_TESTCHAT, mdata);
             IMessagePool<TESTChatMessage>.recycle(mdata);
         }
