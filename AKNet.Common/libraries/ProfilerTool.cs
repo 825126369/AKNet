732b86ad0582fd0c0ee0e82617661b69c729debb
xuke <825126369@qq.com>
2025-11-29 03:33:16 +0800

    2

--- AKNet.Common/libraries/ProfilerTool.cs
+++ AKNet.Common/libraries/ProfilerTool.cs
@@ -11,6 +11,12 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("AKNet")]
+[assembly: InternalsVisibleTo("AKNet.LinuxTcp")]
+[assembly: InternalsVisibleTo("AKNet.MSQuic")]
+[assembly: InternalsVisibleTo("AKNet2")]
+[assembly: InternalsVisibleTo("AKNet.Other")]
 
 namespace AKNet.Common
 {
