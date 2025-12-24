61a9895f389bc9a0820f8374ee6a0fce264af78b
xuke <825126369@qq.com>
2025-12-24 22:26:50 +0800

    2

--- src/AKNet.Common/T4Generator/BuildConst.g.cs
+++ src/AKNet.Common/T4Generator/BuildConst.g.cs
@@ -1,11 +1,13 @@
 ﻿
+
+
 using System;
 
 namespace AKNet.Common
 {
     public static class VersionPublishConfig
     {
-        public static readonly DateTime m_BuildTime = new DateTime(2025, 12, 24, 21, 37, 46);
+        public static readonly DateTime m_BuildTime = new DateTime(2025, 12, 24, 22, 26, 12);
         public static readonly System.Version m_Version = new System.Version(0, 0, 0);
     }
 }
