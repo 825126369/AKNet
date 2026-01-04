8a2eedd09304d1df26906f0a167d32a38abc2203
xuke <825126369@qq.com>
2026-01-04 19:52:40 +0800

    Update ValueTaskSource.cs
    op

--- src/AKNet.Common/Task/ValueTaskSource.cs
+++ src/AKNet.Common/Task/ValueTaskSource.cs
@@ -21,22 +21,19 @@ namespace AKNet.Common
 {
     internal sealed class ValueTaskSource : IValueTaskSource
     {
-        private enum State : byte
-        {
-            None,
-            Awaiting,
-            Completed
-        }
-
-        private State _state;
+        private const int State_None = 1; //初始化一个ValueTask
+        private const int State_Awaiting = 2; //等待设置结果
+        private const int State_Completed = 3; //设置结果完成
+        
         private ManualResetValueTaskSourceCore<bool> _valueTaskSource;
         private CancellationTokenRegistration _cancellationRegistration;
         private GCHandle _keepAlive;
         private readonly object lock_obj = new object();
+        private int _state;
 
         public ValueTaskSource()
         {
-            _state = State.None;
+            _state = State_None;
             _valueTaskSource = new ManualResetValueTaskSourceCore<bool>() { RunContinuationsAsynchronously = true };
             _cancellationRegistration = default;
         }
@@ -45,40 +42,46 @@ namespace AKNet.Common
         {
             get
             {
-                return (State)Volatile.Read(
-                    ref MemoryMarshal.GetReference(MemoryMarshal.Cast<State, byte>(MemoryMarshal.CreateSpan(ref _state, 1)))) == 
-                    State.Completed;
+                return Volatile.Read(ref _state) == State_Completed;
             }
         }
 
-        public bool IsCompletedSuccessfully => IsCompleted && _valueTaskSource.GetStatus(_valueTaskSource.Version) == ValueTaskSourceStatus.Succeeded;
+        public bool IsCompletedSuccessfully => IsCompleted && 
+            _valueTaskSource.GetStatus(_valueTaskSource.Version) == ValueTaskSourceStatus.Succeeded;
 
         public bool TryInitialize(out ValueTask valueTask, object? keepAlive = null, CancellationToken cancellationToken = default)
         {
             lock (lock_obj)
             {
                 valueTask = new ValueTask(this, _valueTaskSource.Version);
-                if (_state == State.None)
+                if (_state == State_None)
                 {
+                    //获取此标记是否能够处于已取消状态。
+                    //CanBeCanceled == false 表示：这个 Token 永远也不会被取消，
+                    //因为它关联的是一个 没有取消源的 CancellationToken.None 或者
+                    //是一个 已经完成的 CancellationTokenSource
                     if (cancellationToken.CanBeCanceled)
                     {
+                        //如果这个调用 cancellationToken 执行了取消操作，就把当前这个 ValueTaskSource 用 OperationCanceledException 设为失败状态。
+                        //当取消的时候, 执行这个委托。
                         _cancellationRegistration = cancellationToken.Register((obj) =>
                         {
+                            //这个!的意思: 我知道这个表达式可能为 null，但我现在向你保证它不会为 null，请不要再给我 CS8600/CS8602 等 nullable警告。
                             ValueTaskSource thisRef = (ValueTaskSource)obj!;
                             thisRef.TrySetException(new OperationCanceledException(cancellationToken));
                         }, this);
                     }
                 }
 
-                if (_state == State.None)
+                if (_state == State_None)
                 {
                     if (keepAlive != null)
                     {
                         Debug.Assert(!_keepAlive.IsAllocated);
-                        _keepAlive = GCHandle.Alloc(keepAlive);
+                        _keepAlive = GCHandle.Alloc(keepAlive); //防止被GC
                     }
 
-                    _state = State.Awaiting;
+                    _state = State_Awaiting;
                     return true;
                 }
                 else
@@ -97,10 +100,10 @@ namespace AKNet.Common
                 {
                     try
                     {
-                        State state = _state;
-                        if (state != State.Completed)
+                        int state = _state;
+                        if (state != State_Completed)
                         {
-                            _state = State.Completed;
+                            _state = State_Completed;
                             cancellationRegistration = _cancellationRegistration;
                             _cancellationRegistration = default;
 
@@ -130,7 +133,7 @@ namespace AKNet.Common
             }
             finally
             {
-                cancellationRegistration.Dispose();
+                cancellationRegistration.Dispose(); //完成之后，把这个注册的委托销毁掉
             }
         }
         
