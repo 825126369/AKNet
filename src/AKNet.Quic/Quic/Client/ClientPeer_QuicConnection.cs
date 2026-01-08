1e582e962e9d43d42a4469fa6d174f874a53e652
xuke <825126369@qq.com>
2026-01-08 22:46:22 +0800

    op

--- src/AKNet.Quic/Quic/Client/ClientPeer_QuicConnection.cs
+++ src/AKNet.Quic/Quic/Client/ClientPeer_QuicConnection.cs
@@ -8,7 +8,6 @@
 *        Copyright:MIT软件许可证
 ************************************Copyright*****************************************/
 using AKNet.Common;
-using AKNet.Quic.Server;
 using System.Net;
 using System.Net.Quic;
 using System.Net.Security;
