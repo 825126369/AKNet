f5500bf81e687e2c24b948c6fdba66d884dcaaa6
xuke <825126369@qq.com>
2026-01-10 14:51:15 +0800

    op

--- src/Test/AkNet.QuicTest/TestNetServer/QuicTestServerBase.cs
+++ src/Test/AkNet.QuicTest/TestNetServer/QuicTestServerBase.cs
@@ -1,5 +1,6 @@
 ﻿using AKNet.Common;
 using AKNet.Extentions.Protobuf;
+using System.Diagnostics;
 using TestCommon;
 using TestProtocol;
 
@@ -11,6 +12,7 @@ namespace TestNetServer
         const int NetCommand_COMMAND_TESTCHAT = 1000;
         public const int nSingleClientStreamCount = 2;
         public abstract QuicServerMainBase Create();
+        int nReceivePackageCount;
 
         public void Start()
         {
@@ -34,11 +36,17 @@ namespace TestNetServer
         private void ReceiveChatMessage(QuicClientPeerBase peer, QuicNetPackage mPackage)
         {
             TESTChatMessage mdata = Proto3Tool.GetData<TESTChatMessage>(mPackage);
+            nReceivePackageCount++;
             for (byte i = 1; i <= nSingleClientStreamCount; i++)
             {
                 peer.SendNetData(i, NetCommand_COMMAND_TESTCHAT, mdata);
             }
             IMessagePool<TESTChatMessage>.recycle(mdata);
+
+            if (nReceivePackageCount % 10000 == 0)
+            {
+                NetLog.Log($"接受包数量: {nReceivePackageCount}");
+            }
         }
     }
 }
