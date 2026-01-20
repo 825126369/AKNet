6f91e6e3b6e613d9eb2871a89a670df4f1f65be4
xuke <825126369@qq.com>
2026-01-20 18:38:08 +0800

    op

--- src/AKNet.Quic/Quic/Client/ClientPeer_QuicStream.cs
+++ src/AKNet.Quic/Quic/Client/ClientPeer_QuicStream.cs
@@ -18,7 +18,7 @@ namespace AKNet.Quic.Client
         private bool _disposed;
         private readonly Memory<byte> mReceiveBuffer = new byte[Config.nIOContexBufferLength];
         private readonly Memory<byte> mSendBuffer = new byte[Config.nIOContexBufferLength];
-        private readonly AkCircularBuffer mSendStreamList = new AkCircularBuffer();
+        private readonly AkCircularManyBuffer mSendStreamList = new AkCircularManyBuffer();
         private readonly NetStreamCircularBuffer mReceiveStreamList = new NetStreamCircularBuffer();
         private bool bSendIOContextUsed = false;
 
