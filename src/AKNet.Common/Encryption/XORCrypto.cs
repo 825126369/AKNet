019c8e5cf1412720632d1443d5b1266a60371abf
xuke <825126369@qq.com>
2026-02-08 15:45:54 +0800

    op

--- src/AKNet.Common/Encryption/XORCrypto.cs
+++ src/AKNet.Common/Encryption/XORCrypto.cs
@@ -19,10 +19,10 @@ namespace AKNet.Common
         {
             //时间戳也不行。因为不同时区，相同的 DateTime, 返回的时间戳不一样。
             //直接 m_BuildTime.ToString() 也不行。在不同的应用上，虽然DateTime 一样，但转为ToString() 后，字符串不一样。
-            if (VersionPublishConfig.m_BuildTime.Day % 2 == 0)
+            if (VersionPublishConfig.m_BuildTime.Day % 2 == 1)
             {
                 //具体化格式化字符串
-                string t = VersionPublishConfig.m_BuildTime.ToString("yyyy/mm/dd HH:mm:ss");
+                string t = VersionPublishConfig.m_BuildTime.ToString("yyyy/MM/dd HH:mm:ss");
                 key = new byte[t.Length];
                 EndianBitConverter.SetBytes(key, 0, t);
             }
@@ -30,7 +30,7 @@ namespace AKNet.Common
             {
                 //不使用时间戳
                 key = new byte[8];
-                var mTimeSpan = VersionPublishConfig.m_BuildTime - new DateTime(2000, 1, 1, 0, 0, 0);
+                var mTimeSpan = VersionPublishConfig.m_BuildTime - DateTime.MinValue;
                 EndianBitConverter.SetBytes(key, 0, (long)mTimeSpan.TotalMilliseconds);
             }
         }
--- src/AKNet.Common/T4Generator/BuildConst.g.cs
+++ src/AKNet.Common/T4Generator/BuildConst.g.cs
@@ -1,12 +1,4 @@
-﻿/************************************Copyright*****************************************
-*        ProjectName:AKNet
-*        Web:https://github.com/825126369/AKNet
-*        Description:C#游戏网络库
-*        Author:许珂
-*        StartTime:2024/11/01 00:00:00
-*        ModifyTime:2026/2/1 20:26:46
-*        Copyright:MIT软件许可证
-************************************Copyright*****************************************/
+﻿
 
 
 using System;
@@ -17,8 +9,9 @@ namespace AKNet.Common
     {
         static VersionPublishConfig()
         {
-            m_BuildTime = new DateTime(2026, 1, 25, 1, 33, 16);
+            m_BuildTime = new DateTime(2026, 2, 8, 15, 40, 36);
             m_Version = new System.Version(0, 0, 0);
         }
+
     }
 }
